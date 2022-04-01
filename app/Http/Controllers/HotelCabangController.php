a6f492f674f7df4215e185808e9b3fe67adef63a
Misbahur <misbahur.rifqi61@gmail.com>
2022-04-01 08:41:27 +0700

    done migration cabang hotels

---
+++ app/Http/Controllers/HotelCabangController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hotel_cabang;
+use Illuminate\Http\Request;
+
+class HotelCabangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Hotel_cabang  $hotel_cabang
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hotel_cabang $hotel_cabang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Hotel_cabang  $hotel_cabang
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hotel_cabang $hotel_cabang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Hotel_cabang  $hotel_cabang
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hotel_cabang $hotel_cabang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Hotel_cabang  $hotel_cabang
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hotel_cabang $hotel_cabang)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_04_01_013858_create_hotel_cabangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotel_cabangs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hotels_id')->nullable()->index('hotels_id_fk3_idx');
+            $table->string('nama');
+            $table->string('alamat', 25);
+            $table->string('created_by');
+            $table->enum('status', ['aktif', 'non_aktif']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotel_cabangs');
+    }
+};
