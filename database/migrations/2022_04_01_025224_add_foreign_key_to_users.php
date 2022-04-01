7e7f2d47039fe871346c5111765332a0cfe1f83b
Misbahur <misbahur.rifqi61@gmail.com>
2022-04-01 09:57:32 +0700

    add foreignkey user

---
+++ database/migrations/2022_04_01_025224_add_foreign_key_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->foreign('roles_id', 'roles_id_fk1')->references('id')->on('roles')->onUpdate('CASCADE')->onDelete('RESTRICT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropForeign('roles_id_fk1');
+        });
+    }
+};
